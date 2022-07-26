3d5400cf41f152362e56cd7981788aba688c0b6b
Yu Heng Lee <lee2000yh@gmail.com>
2022-07-26 19:11:03 +0800

    changed footer

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer className="py-6 text-center text-gray-500 text-sm">
-      <span className="dark:text-gray-100 text-gray-900 font-bold text-lg mr-2">Kek</span>&copy; {new Date().getFullYear()} All Rights gone
+      <span className="dark:text-gray-100 text-gray-900 font-bold text-lg mr-2">Totally legit</span>&copy; {new Date().getFullYear()} Lee Yu Heng
     </footer>
   )
 }
